f007786d16f7a9ca84abff37ab3b3dc6194b5a07
Henrique Sampaio Lira <henriquelira23@outlook.com>
2021-05-25 20:14:10 -0300

    Correção
    Correção de bug onde resultado do cálculo aparecia e sumia depois de clicar em calcular.

--- CalculadoraDeChurrasco/script.js
+++ CalculadoraDeChurrasco/script.js
@@ -4,28 +4,13 @@ var inputTempo = document.getElementById("tempo");
 var resultado = document.getElementsByTagName("div")[1];
 
 function calc(){
-
     let adultos = inputAdultos.value;
     let criancas = inputCriancas.value;
     let tempo = inputTempo.value;
-
     let resultCarne = carneExc(tempo) * adultos + (carneExc(tempo)*criancas/2);
     let resultCerv = cervExc(tempo) * adultos;
     let resultBeb = bebExc(tempo) * adultos + (bebExc(tempo)*criancas/2)
-
-  
-    let p1 = document.createElement("p");
-    let ptext1 = document.createTextNode((resultCarne/1000) + "kg de carne");
-    p1.appendChild(ptext1);
-    let p2 = document.createElement("p");
-    let ptext2 = document.createTextNode(Math.ceil(resultCerv/355)+ " latas de cerveja");
-    p1.appendChild(ptext2);
-    let p3 = document.createElement("p");
-    let ptext3 = document.createTextNode((resultBeb/2000) + " Pet's de refrigerante");
-    p1.appendChild(ptext3);
-    resultado.insertAdjacentElement('afterbegin',p1,p2,p3);
-    resultado.document.write("dasdsa")
-
+    texts(resultCarne,resultCerv,resultBeb);
 }
 function carneExc(tempo){
     if (tempo >= 6) {
@@ -44,4 +29,16 @@ function bebExc(tempo){
         return 1500;
     } else
         return 1000;
+}
+function texts(resultCarne,resultCerv,resultBeb){
+    let p1 = document.createElement("p");
+    let ptext1 = document.createTextNode((resultCarne/1000) + "kg de carne");
+    p1.appendChild(ptext1);
+    let p2 = document.createElement("p");
+    let ptext2 = document.createTextNode(Math.ceil(resultCerv/355)+ " latas de cerveja");
+    p1.appendChild(ptext2);
+    let p3 = document.createElement("p");
+    let ptext3 = document.createTextNode((resultBeb/2000) + " Pet's de refrigerante");
+    p1.appendChild(ptext3);
+    resultado.insertAdjacentElement('afterbegin',p1,p2,p3);
 }
